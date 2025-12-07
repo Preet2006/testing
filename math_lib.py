f97bbf32cfa0fe646983c8d214eddf54410a00fd
Preet2006 <preet.jhagekar@gmail.com>
2025-12-07 18:09:07 +0530

    Initial commit with buggy code

---
+++ math_lib.py
@@ -0,0 +1,12 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def divide(a, b):
+    # TODO: Fix this, it crashes if b is 0
+    return a / b
+
+def multiply(a, b):
+    return a * b
