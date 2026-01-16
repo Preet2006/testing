0dfbab433d120a65d7c1ce098306dadacdb0e628
Preet2006 <preet.jhagekar@gmail.com>
2026-01-16 19:36:43 +0530

    Security fix for issue #6: Insecure deserialization

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
-import pickle
+import json
 import base64
+
 def load_session(token):
-    # VULNERABILITY: Unpickling untrusted data
     data = base64.b64decode(token)
-    return pickle.loads(data)
+    try:
+        return json.loads(data.decode())
+    except json.JSONDecodeError:
+        raise ValueError("Invalid session token")
