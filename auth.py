6e6850506c50afd82c24ffa5575f0bab48b09279
Preet Jhagekar <147617586+Preet2006@users.noreply.github.com>
2026-01-18 10:36:24 +0530

    Delete auth.py

--- auth.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-def login(username, password):
-    conn = sqlite3.connect('users.db')
-    # VULNERABILITY: Raw SQL String Concatenation
-    query = "SELECT * FROM users WHERE user = '" + username + "' AND pass = '" + password + "'"
-    cursor = conn.execute(query)
-    return cursor.fetchone()
